3c4bfb344de82cee0b1be2f73046ce222038771e
rhithik <rhithiktheawesome@gmail.com>
2021-01-24 15:14:16 -0500

    added submit buttons to forms

--- index.php
+++ index.php
@@ -25,12 +25,14 @@
 	<form title="Add Income" action="">
 		<p>Amount: <input type="text" aria-label="Enter income amount"></p>
 		<p>Date: <input type="date" aria-label="Enter date of income"></p>
+		<submit>Add Income</submit>
 	</form>
 	<!-- Expenses form for adding expenses to budget, pops up after clicking Add
 	Expenses button -->
 	<form title="Add Expenses" action="">
 		<p>Amount: <input type="text" aria-label="Enter amount of expense"></p>
 		<p>Date Due: <input type="date" aria-label="Enter when expense is due"></p>
+		<submit>Add Expense</submit>
 	</form>
 </div>
 </body>
